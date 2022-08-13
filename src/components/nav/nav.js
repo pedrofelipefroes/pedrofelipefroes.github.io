f66b26743adb2b2a72bc6e5a8d0bac430c2612d4
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-08-13 01:46:19 -0300

    Adding dynamic visibility to Nav.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import c from "classnames"
 
 import NavigationLink from "../navigation-link/navigation-link"
@@ -6,9 +6,31 @@ import NavigationLink from "../navigation-link/navigation-link"
 const Nav = () => {
     const ref = useRef(null)
     const [expanded, setExpanded] = useState(false)
+    const [scrolled, setScrolled] = useState(null)
+    const [visible, setVisible] = useState(true)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const previousScrollPos = scrolled
+            const currentScrollPos = window.pageYOffset
+
+            console.log("previous: " + previousScrollPos)
+            console.log("current: " + currentScrollPos)
+
+            setVisible(previousScrollPos > currentScrollPos || currentScrollPos <= 0)
+            setScrolled(currentScrollPos)
+        }
+
+        setScrolled(window.pageYOffset)
+        document.addEventListener("scroll", handleScroll, { passive: true })
+
+        return () => {
+            document.removeEventListener("scroll", handleScroll)
+        }
+    }, [scrolled])
 
     return (
-        <nav className={c("c-nav", { "c-nav--expanded": expanded })}>
+        <nav className={c("c-nav", { "c-nav--expanded": expanded, "c-nav--visible": visible })}>
             <div className="c-nav__header">
                 <NavigationLink to="/">froes.design</NavigationLink>
                 <svg className={c("icon c-nav__icon", { "c-nav__icon--expanded": expanded })} ref={ref} onClick={() => setExpanded(!expanded)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
