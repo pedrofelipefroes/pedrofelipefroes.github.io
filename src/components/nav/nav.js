5ca1b08fb8540a58bd944ef9546485a90635668d
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-08-13 02:22:33 -0300

    Fixing a bug in Nav component.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,27 +1,33 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import c from "classnames"
 
 import NavigationLink from "../navigation-link/navigation-link"
 
 const Nav = () => {
-    const ref = useRef(null)
     const [expanded, setExpanded] = useState(false)
     const [scrolled, setScrolled] = useState(null)
     const [visible, setVisible] = useState(true)
+
+    const handleClick = () => {
+        setExpanded(!expanded)
+        document.querySelector("body").style.overflow = "revert"
+
+        if (!expanded) {
+            document.querySelector("body").style.overflow = "hidden"
+        }
+    }
 
     useEffect(() => {
         const handleScroll = () => {
             const previousScrollPos = scrolled
             const currentScrollPos = window.pageYOffset
 
-            console.log("previous: " + previousScrollPos)
-            console.log("current: " + currentScrollPos)
-
             setVisible(previousScrollPos > currentScrollPos || currentScrollPos <= 0)
             setScrolled(currentScrollPos)
         }
 
         setScrolled(window.pageYOffset)
+
         document.addEventListener("scroll", handleScroll, { passive: true })
 
         return () => {
@@ -33,7 +39,7 @@ const Nav = () => {
         <nav className={c("c-nav", { "c-nav--expanded": expanded, "c-nav--visible": visible })}>
             <div className="c-nav__header">
                 <NavigationLink to="/">froes.design</NavigationLink>
-                <svg className={c("icon c-nav__icon", { "c-nav__icon--expanded": expanded })} ref={ref} onClick={() => setExpanded(!expanded)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className={c("icon c-nav__icon", { "c-nav__icon--expanded": expanded })} onClick={handleClick} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <line x1="1" y1="9.5" x2="23" y2="9.5" />
                     <line x1="1" y1="13.5" x2="23" y2="13.5" />
                 </svg>
