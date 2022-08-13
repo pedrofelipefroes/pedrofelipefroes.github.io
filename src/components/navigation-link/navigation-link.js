a4806ce4e8d42956d688b8642c63bd81390b15ad
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-08-13 00:05:36 -0300

    Updating styling of Navigation Link.

--- src/components/navigation-link/navigation-link.js
+++ src/components/navigation-link/navigation-link.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const NavigationLink = ({ children, hasIcon, to, ...other }) => {
     const internal = /^\/(?!\/)/.test(to)
     const icon = hasIcon ?
-        <svg className="c-navigation-link__icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="icon c-navigation-link__icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <line x1="1" y1="11.5" x2="23" y2="11.5" />
             <g className="arrowhead">
                 <path d="M15.5 16C15.5 16 15.5 11.5 21.5 11.5H23" />
@@ -16,7 +16,7 @@ const NavigationLink = ({ children, hasIcon, to, ...other }) => {
 
     if (internal) {
         return (
-            <Link className="c-navigation-link" to={to} {...other}>
+            <Link className="c-navigation-link text-link" to={to} {...other}>
                 {icon} {children}
             </Link>
         )
