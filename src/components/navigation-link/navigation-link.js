59677f2b5d60eb6df61577fecace25d94ba27f03
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-08-20 01:20:30 -0300

    Updating Navigation Link to use CSS Modules.

--- src/components/navigation-link/navigation-link.js
+++ src/components/navigation-link/navigation-link.js
@@ -1,13 +1,16 @@
 import * as React from "react"
+import c from "classnames"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { arrowhead, navigationLink } from "./navigation-link.module.css"
 
 const NavigationLink = ({ classNames, children, hasIcon, to, ...other }) => {
     const internal = /^\/(?!\/)/.test(to)
+    const className = c(navigationLink, "color-on-bg font-medium font-monospace text-caption text-uppercase tracking-wider")
     const icon = hasIcon ?
-        <svg className="icon c-navigation-link__icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="icon spacing-inline-sm" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <line x1="1" y1="11.5" x2="23" y2="11.5" />
-            <g className="arrowhead">
+            <g className={arrowhead}>
                 <path d="M15.5 16C15.5 16 15.5 11.5 21.5 11.5H23" />
                 <path d="M15.5 7C15.5 7 15.5 11.5 22 11.5H23" />
             </g>
@@ -16,13 +19,14 @@ const NavigationLink = ({ classNames, children, hasIcon, to, ...other }) => {
 
     if (internal) {
         return (
-            <Link className="c-navigation-link text-link" to={to} {...other}>
+            <Link className={className} to={to} {...other}>
                 {icon} {children}
             </Link>
         )
     }
+
     return (
-        <a className={classNames ? "c-navigation-link text-link " + classNames : "c-navigation-link text-link"} href={to} {...other}>
+        <a className={classNames ? className + classNames : className} href={to} {...other}>
             {icon} {children}
         </a>
     )
