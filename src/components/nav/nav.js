179c9612c9505e337304ab85ef7097164445d596
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-09-04 14:08:33 -0300

    Updating Nav component.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react"
 import c from "classnames"
+
+import { AnchorLink } from "gatsby-plugin-anchor-links"
+import { StaticImage } from "gatsby-plugin-image"
 import NavigationLink from "../navigation-link/navigation-link"
-import { nav, navContent, navFooter, navHeader, navIcon, navIsExpanded, navIsVisible, navLink } from "./nav.module.css"
+
+import { nav, navBody, navFooter, navHeader, navIcon, navIsExpanded, navIsVisible } from "./nav.module.css"
 
 
 const Nav = () => {
@@ -18,9 +22,9 @@ const Nav = () => {
         }
     }
 
-    const menuIcon = <svg className={c(navIcon + " icon", { [navIsExpanded]: expanded })} onClick={handleClick} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line x1="1" y1="9.5" x2="23" y2="9.5" />
-        <line x1="1" y1="13.5" x2="23" y2="13.5" />
+    const icon = <svg className={c(navIcon, "icon js-hoverable-link", { [navIsExpanded]: expanded })} onClick={handleClick} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="5" y1="12.5" x2="25" y2="12.5" />
+        <line x1="5" y1="16.5" x2="25" y2="16.5" />
     </svg>
 
     useEffect(() => {
@@ -43,25 +47,33 @@ const Nav = () => {
 
     return (
         <nav className={c(nav, { [navIsExpanded]: expanded, [navIsVisible]: visible })}>
-            <div className={c(navHeader, "color-bg height-8 spacing-inset-squish-md", { [navIsExpanded]: expanded })}>
-                <NavigationLink to="/">froes.design</NavigationLink>
-                {menuIcon}
+            <div className={c(navHeader, "divider sp-inset-x", { [navIsExpanded]: expanded })}>
+                <NavigationLink to="/">Froes.design</NavigationLink>
+                {icon}
                 <ul>
-                    <li>
-                        <NavigationLink classNames="spacing-inline-md" to="#about">About</NavigationLink>
+                    <li className="sp-inline-md">
+                        <NavigationLink to="/#about">About</NavigationLink>
                     </li>
                     <li>
-                        <NavigationLink to="#contact">Contact</NavigationLink>
+                        <NavigationLink to="/#contact">Contact</NavigationLink>
                     </li>
                 </ul>
             </div>
-            <div className={c(navContent, "height-full", { [navIsExpanded]: expanded })}>
-                <NavigationLink to="#about" onClick={handleClick}><h2 className={c(navLink, "height-fulls text-center text-title-1 width-full", { [navIsExpanded]: expanded })}>About</h2></NavigationLink>
-                <NavigationLink to="#contact" onClick={handleClick}><h2 className={c(navLink, "height-full text-center text-title-1 width-full", { [navIsExpanded]: expanded })}>Contact</h2></NavigationLink>
+            <div className={c(navBody, { [navIsExpanded]: expanded })}>
+                <div className="divider sp-inset-x">
+                    <AnchorLink to="/#about" className="color-on-bg text-decoration-none text-ms-3">
+                        <span onClick={handleClick}>About</span>
+                    </AnchorLink>
+                </div>
+                <div className="divider sp-inset-x">
+                    <AnchorLink to="/#contact" className="color-on-bg text-decoration-none text-ms-3">
+                        <span onClick={handleClick}>Contact</span>
+                    </AnchorLink>
+                </div>
             </div>
-            <div className={c(navFooter, "font-sans-serif text-body spacing-inset-squish-md", { [navIsExpanded]: expanded })}>
-                <p className={c({ [navIsExpanded]: expanded })}>São Paulo, Brazil.</p>
-                <p className={c("text-right", { [navIsExpanded]: expanded })}>Froes © 2022.</p>
+            <div className={c(navFooter, "divider sp-inset-x", { [navIsExpanded]: expanded })}>
+                São Paulo, 2022.
+                <StaticImage src="../../images/signature.png" alt="Froes handwritten signature in black." formats={["auto", "webp", "avif"]} layout="fixed" loading="eager" quality={100} width={58} />
             </div>
         </nav>
     )
