15f1418be662126a5dcb0db60622efa751a17c4a
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-09-04 22:23:33 -0300

    Updating 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
 import * as React from "react"
+import c from "classnames"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout/layout"
+import Seo from "../components/seo/seo"
+
+import { notFound } from "../pages/not-found.module.css"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Seo title="404: Not Found" />
+    <section className={c(notFound, "divider grid-golden-ratio sp-inset-x sp-inset-stack-md")}>
+      <div>
+        <h1 className="sp-stack-xs text-ms-4">Not all those who wander are lost.</h1>
+        <p className="text-ms-1">
+          A lyric from an <a href="https://open.spotify.com/track/0Z3JiHn6mhcObPmKhV9WRW?si=DGv5aAaWQG6Vjr0DbF4VBw" className="color-on-bg js-interactable-link text-nowrap">homonymous song</a> from Lana Del Rey.
+        </p>
+      </div>
+      <p className="text-ms-0">
+        (But you are definetly lost. <AnchorLink to="/" className="color-on-bg js-interactable-link text-nowrap">Go back home.</AnchorLink>)
+      </p>
+    </section>
   </Layout>
 )
 
