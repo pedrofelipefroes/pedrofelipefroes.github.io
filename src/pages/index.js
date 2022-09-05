bb923e3cca2bcc49baa37b1d8f85b6160bcdcaa2
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-09-04 21:39:20 -0300

    Updating Index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,43 @@
 import * as React from "react"
 import c from "classnames"
-import { Parallax, ParallaxProvider } from "react-scroll-parallax"
+import { Parallax } from "react-scroll-parallax"
 
 import Contact from "../components/contact/contact"
-import Cursor from "../components/cursor/cursor"
-import Footer from "../components/footer/footer"
-import Nav from "../components/nav/nav"
+import Layout from "../components/layout/layout"
 import NavigationLink from "../components/navigation-link/navigation-link"
+import Seo from "../components/seo/seo"
 
-import { indexAbout, indexHeader, indexHeaderLinks } from "./index.module.css"
+import { indexAbout, indexHeader } from "./index.module.css"
 
 const IndexPage = () => (
-  <ParallaxProvider>
-    <main>
-      <header className={indexHeader}>
-        <Nav />
-        <div className="grid-golden-ratio">
-          <h1 className="font-sans-serif text-title-2">Froes is a designer and engineer with a knack for <span className="text-nowrap">systematic design.</span></h1>
-          <div className={indexHeaderLinks}>
-            <NavigationLink classNames="spacing-stack-sm" to="#about">About</NavigationLink>
-            <br />
-            <NavigationLink to="#contact">Contact</NavigationLink>
-          </div>
+  <>
+    <Layout>
+      <Seo title="FROES.DESIGN" />
+      <header className={c(indexHeader, "grid-golden-ratio sp-inset-x sp-inset-stack-lg")}>
+        <h1 className="text-ms-1">
+          Froes is a designer and engineer with a knack for <span className="text-nowrap">systematic design.</span>
+        </h1>
+        <div>
+          <NavigationLink to="/#about" className="sp-stack-xs">About</NavigationLink>
+          <br />
+          <NavigationLink to="/#contact">Contact</NavigationLink>
         </div>
       </header>
-      <section className={c(indexAbout, "grid-golden-ratio spacing-inset-squish-lg")} id="#about">
-        <Parallax speed={-3}>
-          <h2 className="font-regular font-sans-serif text-title-1 tracking-narrow">
-            Current <em className="">Head of Design Systems</em> at Postclick. Former <em className="">Design Lead</em> at Avenue Code.
-          </h2>
+      <section id="#about" className={c(indexAbout, "divider grid-golden-ratio sp-inset-x sp-inset-stack-md")}>
+        <Parallax speed={-5}>
+          <h2 className="text-ms-3">Current Head of Design Systems at Postclick. Former Design Lead at Avenue Code.</h2>
         </Parallax>
-        <div className="font-regular font-sans-serif text-body">
-          <p className="spacing-stack-sm">I'm a designer and computer engineer with over six years of experience who really (really) enjoys a systematic approach <span className="text-nowrap">to design.</span></p>
-          <p className="spacing-stack-sm">This means I'm right at home when dealing with design systems, developing <span className="font-medium font-smcps">CSS</span> architectures, crafting typography scales, writing documentation, and finding <span className="text-nowrap">patterns everywhere.</span></p>
-          <p>Find below some work samples and <span className="text-nowrap">experience information.</span></p>
+        <div className="justify-self-end text-ms-0">
+          <p className="sp-inset-stack-xs">I'm a designer and computer engineer with over six years of experience who really (really) enjoys a systematic approach <span className="text-nowrap">to design.</span></p>
+          <p>This means I'm right at home when dealing with design systems, developing <span className="text-smcps">CSS</span> architectures, crafting typography scales, writing documentation, and finding <span className="text-nowrap">patterns everywhere.</span></p>
         </div>
-        <div className="grid-golden-ratio--column-end">
-          <NavigationLink classNames="spacing-stack-sm" to="https://www.dropbox.com/s/qrps0a0p1zc745k/Froes%20%E2%80%94%20Design%20Portfolio.pdf?dl=0">Samples (WIP)</NavigationLink>
-          <br />
-          <NavigationLink to="https://www.dropbox.com/s/s3ptwqbbnumlrn9/Designer_PedroFroes.pdf?dl=0">Resumé (WIP)</NavigationLink>
+        <div>
+          <NavigationLink to="https://www.dropbox.com/s/qrps0a0p1zc745k/Froes%20%E2%80%94%20Design%20Portfolio.pdf?dl=0">Work Samples (WIP)</NavigationLink>
         </div>
       </section>
       <Contact />
-      <Footer />
-    </main>
-    <Cursor />
-  </ParallaxProvider>
+    </Layout>
+  </>
 )
 
 export default IndexPage
