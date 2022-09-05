ee18bbb2028e849ed425355a3cd50e3371aed306
pedrofelipefroes <pedrofelipefroes+github@gmail.com>
2022-09-04 21:33:23 -0300

    Updating Contact component.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,37 +1,47 @@
 import React from "react"
 import c from "classnames"
 import { Parallax } from "react-scroll-parallax";
-import { contact } from "./contact.module.css"
 
 import NavigationLink from "../navigation-link/navigation-link"
 
-const Contact = () => {
-    const headlineCopy = "Reach Out"
+import { contact, contactHeadline } from "./contact.module.css"
 
-    let headlineParallax = headlineCopy.split("").map((i) => {
-        return (
-            <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateX={[(-headlineCopy.length * 20 + headlineCopy.indexOf(i) * 10), 0]}>
-                {headlineCopy.indexOf(i) === 5 ? <span>&nbsp;</span> : <span>{i}</span>}
-            </Parallax>
-        )
-    })
+const Contact = () => {
+    const splitWord = (word, phrase) => {
+        return word.split("").map((i) => {
+            return (
+                <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateX={[(-20 * phrase.length + 10 * word.indexOf(i)), 0]}>
+                    <span>{i}</span>
+                </Parallax>
+            )
+        })
+    }
+
+    const headline = "Reach Out"
+    const headlineStart = headline.split(" ")[0]
+    const headlineEnd = headline.split(" ")[1]
+
+    const headlineParallax = <>
+        <div className={contactHeadline}>{splitWord(headlineStart, headline)}</div>
+        <div className={contactHeadline}>{splitWord(headlineEnd, headline)}</div>
+    </>
 
     return (
-        <section className={c(contact, "grid-golden-ratio spacing-inset-squish-lg")}>
-            <h2 className="font-sans-serif text-large-title tracking-narrow">
+        <section id="#contact" className={c(contact, "divider grid-golden-ratio sp-inset-x sp-inset-stack-md")}>
+            <h2 className="text-ms-4">
                 {headlineParallax}
             </h2>
             <div>
                 <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateY={[50, 0]}>
-                    <NavigationLink classNames={"spacing-stack-sm"} to="mailto:hello@froes.design">hello@froes.design</NavigationLink>
+                    <NavigationLink className="sp-stack-xs" to="mailto:hello@froes.design">hello@froes.design</NavigationLink>
                 </Parallax>
                 <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateY={[75, 0]}>
-                    <NavigationLink classNames={"spacing-stack-sm"} to="https://www.linkedin.com/in/froesdesign/">in/froesdesign</NavigationLink>
+                    <NavigationLink className="sp-stack-xs" to="https://www.linkedin.com/in/froesdesign/">in/froesdesign</NavigationLink>
                 </Parallax>
                 {/* <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateY={[80, 0]}>
                     <NavigationLink classNames={"spacing-stack-sm"} to="https://www.instagram.com/pedrofelipefroes/">Working Not Working</NavigationLink>
                 </Parallax> */}
-                <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateY={[100, 0]}>
+                <Parallax easing="easeOutQuad" shouldAlwaysCompleteAnimation translateY={[125, 0]}>
                     <NavigationLink to="https://www.instagram.com/pedrofelipefroes/">@pedrofelipefroes</NavigationLink>
                 </Parallax >
             </div>
