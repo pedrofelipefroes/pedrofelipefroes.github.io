6f4f9ee330bd4b0f336ddc22e66465c52164c02f
pedrofelipefroes <>
2022-09-07 15:49:52 -0300

    Adding ExperienceCard component.

---
+++ src/components/experience-card/experience-card.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+
+import { experienceCard } from "./experience-card.module.css"
+
+const ExperienceCard = ({ children, company, role }) => {
+    return (
+        <div className={experienceCard}>
+            <h3 className="sp-stack-xxs text-ms-1">{role}</h3>
+            <p className="text-ms-0">{company}</p>
+            <p className="text-ms-0">
+                {children}
+            </p>
+        </div>
+    )
+}
+
+ExperienceCard.propTypes = {
+    children: PropTypes.node.isRequired,
+    company: PropTypes.string,
+    role: PropTypes.string.isRequired,
+}
+
+export default ExperienceCard
