610983ba665dea41290e438177ca0abd4ef2082e
pedrofelipefroes <>
2022-09-11 00:51:38 -0300

    Adding button component.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types"
 
 import { button } from "./button.module.css"
 
-const Button = ({ children }) => {
+const Button = ({ children, ...props }) => {
     return (
-        <button className={c(button, "color-on-bg js-interactable-button text-ms-0")}>{children}</button>
+        <button className={c(button, "color-on-bg js-interactable-button text-ms-0")} {...props}>{children}</button>
     )
 }
 
