0c0088e1b3879c2d774360b23f9fec3c4ea24c16
pedrofelipefroes <>
2022-09-17 12:54:18 -0300

    Adding SectionTitle component.

---
+++ src/components/section-title/section-title.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import c from "classnames";
+import PropTypes from "prop-types";
+import { Parallax } from "react-scroll-parallax";
+
+import { sectionTitle } from "./section-title.module.css";
+
+const SectionTitle = ({ label, spEnd, spStart }) => {
+	return (
+		<Parallax opacity={[1, 0]}>
+			<h2
+				className={c(
+					sectionTitle,
+					"font-monospace sp-inset-inline text-ms-0 text-uppercase",
+					`sp-inset-block-end-${spEnd}`,
+					`sp-inset-block-start-${spStart}`
+				)}
+			>
+				{label}
+			</h2>
+		</Parallax>
+	);
+};
+
+SectionTitle.propTypes = {
+	label: PropTypes.string.isRequired,
+	spEnd: PropTypes.string,
+	spStart: PropTypes.string,
+};
+
+SectionTitle.defaultProps = {
+	spEnd: "md",
+	spStart: "lg",
+};
+
+export default SectionTitle;
