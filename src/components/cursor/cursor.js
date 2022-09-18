f4a89e416781019d9f0c38ad48310c049a893721
pedrofelipefroes <>
2022-09-18 02:12:29 -0300

    Prettyfying Cursor component.

--- src/components/cursor/cursor.js
+++ src/components/cursor/cursor.js
@@ -1,212 +1,260 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 
-import { cursor, isLocked } from "./cursor.module.css"
+import { cursor, isLocked } from "./cursor.module.css";
 
 const Cursor = () => {
-    const isBrowser = typeof window !== "undefined"
+	const isBrowser = typeof window !== "undefined";
 
-    let x = isBrowser ? window.innerWidth / 2 : null
-    let y = isBrowser ? window.innerHeight / 2 : null
+	let x = isBrowser ? window.innerWidth / 2 : null;
+	let y = isBrowser ? window.innerHeight / 2 : null;
 
-    const ref = useRef(null)
-    const endX = useRef(x)
-    const endY = useRef(y)
-    const [cursorLocked, setCursorLocked] = useState(false)
+	const ref = useRef(null);
+	const endX = useRef(x);
+	const endY = useRef(y);
+	const [cursorLocked, setCursorLocked] = useState(false);
 
-    useEffect(() => {
-        const cursor = ref.current
-        const defaultCursorSize = "1em"
+	useEffect(() => {
+		const cursor = ref.current;
+		const defaultCursorSize = "1em";
 
-        const handleScroll = () => {
-            cursor.style.setProperty("--cursor-scale", 0)
+		const handleScroll = () => {
+			cursor.style.setProperty("--cursor-scale", 0);
 
-            window.setTimeout(() => {
-                cursor.style.setProperty("--cursor-scale", 1)
-            }, 500)
-        }
+			window.setTimeout(() => {
+				cursor.style.setProperty("--cursor-scale", 1);
+			}, 500);
+		};
 
-        const handleMouseDown = () => {
-            if (!cursorLocked) {
-                cursor.style.setProperty("--cursor-scale", .5)
-            }
-        }
+		const handleMouseDown = () => {
+			if (!cursorLocked) {
+				cursor.style.setProperty("--cursor-scale", 0.5);
+			}
+		};
 
-        const handleMouseMove = e => {
-            endX.current = e.pageX
-            endY.current = e.pageY
+		const handleMouseMove = (e) => {
+			endX.current = e.pageX;
+			endY.current = e.pageY;
 
-            if (!cursorLocked) {
-                cursor.style.setProperty("--cursor-top", endY.current + "px")
-                cursor.style.setProperty("--cursor-left", endX.current + "px")
-            }
-        }
+			if (!cursorLocked) {
+				cursor.style.setProperty("--cursor-top", endY.current + "px");
+				cursor.style.setProperty("--cursor-left", endX.current + "px");
+			}
+		};
 
-        const handleMouseUp = () => {
-            if (!cursorLocked) {
-                cursor.style.setProperty("--cursor-scale", 1)
-            }
-        }
+		const handleMouseUp = () => {
+			if (!cursorLocked) {
+				cursor.style.setProperty("--cursor-scale", 1);
+			}
+		};
 
-        const handleLinkMouseEnter = e => {
-            setCursorLocked(true)
+		const handleLinkMouseEnter = (e) => {
+			setCursorLocked(true);
 
-            document.removeEventListener("mousemove", handleMouseMove)
+			document.removeEventListener("mousemove", handleMouseMove);
 
-            cursor.classList.add(isLocked)
+			cursor.classList.add(isLocked);
 
-            let rect = e.target.getBoundingClientRect()
+			let rect = e.target.getBoundingClientRect();
 
-            cursor.style.setProperty("--cursor-top", window.scrollY + rect.top + rect.height / 2 + "px")
-            cursor.style.setProperty("--cursor-left", rect.left + rect.width / 2 + "px")
-            cursor.style.setProperty("--cursor-width", "calc(" + rect.width + "px + 1.2em)")
-            cursor.style.setProperty("--cursor-height", "calc(" + rect.height + "px + .8em)")
+			cursor.style.setProperty(
+				"--cursor-top",
+				window.scrollY + rect.top + rect.height / 2 + "px"
+			);
+			cursor.style.setProperty(
+				"--cursor-left",
+				rect.left + rect.width / 2 + "px"
+			);
+			cursor.style.setProperty(
+				"--cursor-width",
+				"calc(" + rect.width + "px + 1.2em)"
+			);
+			cursor.style.setProperty(
+				"--cursor-height",
+				"calc(" + rect.height + "px + .8em)"
+			);
 
-            e.target.style.setProperty("--cursor-scale", 1.05)
-        }
+			e.target.style.setProperty("--cursor-scale", 1.05);
+		};
 
-        const handleLinkMouseLeave = e => {
-            setCursorLocked(false)
+		const handleLinkMouseLeave = (e) => {
+			setCursorLocked(false);
 
-            document.addEventListener("mousemove", handleMouseMove)
+			document.addEventListener("mousemove", handleMouseMove);
 
-            cursor.classList.remove(isLocked);
+			cursor.classList.remove(isLocked);
 
-            cursor.style.setProperty("--cursor-width", defaultCursorSize)
-            cursor.style.setProperty("--cursor-height", defaultCursorSize)
-            cursor.style.setProperty("--cursor-translateX", 0)
-            cursor.style.setProperty("--cursor-translateY", 0)
+			cursor.style.setProperty("--cursor-width", defaultCursorSize);
+			cursor.style.setProperty("--cursor-height", defaultCursorSize);
+			cursor.style.setProperty("--cursor-translateX", 0);
+			cursor.style.setProperty("--cursor-translateY", 0);
 
-            e.target.style.setProperty("--cursor-translateX", 0)
-            e.target.style.setProperty("--cursor-translateY", 0)
-            e.target.style.setProperty("--cursor-scale", 1)
-        }
+			e.target.style.setProperty("--cursor-translateX", 0);
+			e.target.style.setProperty("--cursor-translateY", 0);
+			e.target.style.setProperty("--cursor-scale", 1);
+		};
 
-        const handleLinkMouseMove = e => {
-            let rect = e.target.getBoundingClientRect()
+		const handleLinkMouseMove = (e) => {
+			let rect = e.target.getBoundingClientRect();
 
-            const halfHeight = rect.height / 2
-            const topOffset = (e.y - rect.top - halfHeight) / halfHeight
-            const halfWidth = rect.width / 2
-            const leftOffset = (e.x - rect.left - halfWidth) / halfWidth
+			const halfHeight = rect.height / 2;
+			const topOffset = (e.y - rect.top - halfHeight) / halfHeight;
+			const halfWidth = rect.width / 2;
+			const leftOffset = (e.x - rect.left - halfWidth) / halfWidth;
 
-            cursor.style.setProperty("--cursor-translateX", `${leftOffset * 3}px`)
-            cursor.style.setProperty("--cursor-translateY", `${topOffset * 3}px`)
+			cursor.style.setProperty("--cursor-translateX", `${leftOffset * 3}px`);
+			cursor.style.setProperty("--cursor-translateY", `${topOffset * 3}px`);
 
-            e.target.style.setProperty("--cursor-translateX", `${leftOffset * 6}px`)
-            e.target.style.setProperty("--cursor-translateY", `${topOffset * 4}px`)
-        }
+			e.target.style.setProperty("--cursor-translateX", `${leftOffset * 6}px`);
+			e.target.style.setProperty("--cursor-translateY", `${topOffset * 4}px`);
+		};
 
-        const handleButtonMouseEnter = e => {
-            setCursorLocked(true)
+		const handleButtonMouseEnter = (e) => {
+			setCursorLocked(true);
 
-            document.removeEventListener("mousemove", handleMouseMove)
+			document.removeEventListener("mousemove", handleMouseMove);
 
-            cursor.classList.add(isLocked)
-            cursor.classList.add("js-has-button-breathe")
+			cursor.classList.add(isLocked);
+			cursor.classList.add("js-has-button-breathe");
 
-            let rect = e.target.getBoundingClientRect()
+			let rect = e.target.getBoundingClientRect();
 
-            cursor.style.setProperty("--cursor-top", window.scrollY + rect.top + rect.height / 2 + "px")
-            cursor.style.setProperty("--cursor-left", rect.left + rect.width / 2 + "px")
-            cursor.style.setProperty("--cursor-width", "calc(" + rect.width + "px + .8em)")
-            cursor.style.setProperty("--cursor-height", "calc(" + rect.height + "px + .8em)")
-            cursor.style.setProperty("--cursor-radius", "4em")
-        }
+			cursor.style.setProperty(
+				"--cursor-top",
+				window.scrollY + rect.top + rect.height / 2 + "px"
+			);
+			cursor.style.setProperty(
+				"--cursor-left",
+				rect.left + rect.width / 2 + "px"
+			);
+			cursor.style.setProperty(
+				"--cursor-width",
+				"calc(" + rect.width + "px + .8em)"
+			);
+			cursor.style.setProperty(
+				"--cursor-height",
+				"calc(" + rect.height + "px + .8em)"
+			);
+			cursor.style.setProperty("--cursor-radius", "4em");
+		};
 
-        const handleButtonMouseLeave = e => {
-            setCursorLocked(false)
+		const handleButtonMouseLeave = (e) => {
+			setCursorLocked(false);
 
-            document.addEventListener("mousemove", handleMouseMove)
+			document.addEventListener("mousemove", handleMouseMove);
 
-            cursor.classList.remove(isLocked);
-            cursor.classList.remove("js-has-button-breathe")
+			cursor.classList.remove(isLocked);
+			cursor.classList.remove("js-has-button-breathe");
 
-            cursor.style.setProperty("--cursor-width", defaultCursorSize)
-            cursor.style.setProperty("--cursor-height", defaultCursorSize)
-            cursor.style.setProperty("--cursor-translateX", 0)
-            cursor.style.setProperty("--cursor-translateY", 0)
-            cursor.style.setProperty("--cursor-radius", "0.6em")
-        }
+			cursor.style.setProperty("--cursor-width", defaultCursorSize);
+			cursor.style.setProperty("--cursor-height", defaultCursorSize);
+			cursor.style.setProperty("--cursor-translateX", 0);
+			cursor.style.setProperty("--cursor-translateY", 0);
+			cursor.style.setProperty("--cursor-radius", "0.6em");
+		};
 
-        const handleTextMouseOut = () => {
-            cursor.style.setProperty("--cursor-width", defaultCursorSize);
-            cursor.style.setProperty("--cursor-height", defaultCursorSize);
-        }
+		const handleTextMouseOut = () => {
+			cursor.style.setProperty("--cursor-width", defaultCursorSize);
+			cursor.style.setProperty("--cursor-height", defaultCursorSize);
+		};
 
-        const handleTextMouseOver = ({ target }) => {
-            const lineHeight = getComputedStyle(target).lineHeight
+		const handleTextMouseOver = ({ target }) => {
+			const lineHeight = getComputedStyle(target).lineHeight;
 
-            cursor.style.setProperty("--cursor-width", "0.15em");
-            cursor.style.setProperty("--cursor-height", "calc(" + lineHeight + " + 0.3em)");
-        }
+			cursor.style.setProperty("--cursor-width", "0.15em");
+			cursor.style.setProperty(
+				"--cursor-height",
+				"calc(" + lineHeight + " + 0.3em)"
+			);
+		};
 
-        document.addEventListener("scroll", handleScroll)
-        document.addEventListener("mousedown", handleMouseDown)
-        document.addEventListener("mousemove", handleMouseMove)
-        document.addEventListener("mouseup", handleMouseUp)
+		document.addEventListener("scroll", handleScroll);
+		document.addEventListener("mousedown", handleMouseDown);
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
 
-        document.querySelectorAll(".js-interactable-link").forEach((link) => {
-            const isHidden = getComputedStyle(link).display === "none"
+		document.querySelectorAll(".js-interactable-link").forEach((link) => {
+			const isHidden = getComputedStyle(link).display === "none";
 
-            if (!isHidden) {
-                link.addEventListener("mouseenter", handleLinkMouseEnter, { passive: true })
-                link.addEventListener("mouseleave", handleLinkMouseLeave, { passive: true })
-                link.addEventListener("mousemove", handleLinkMouseMove, { passive: true })
-            }
-        })
+			if (!isHidden) {
+				link.addEventListener("mouseenter", handleLinkMouseEnter, {
+					passive: true,
+				});
+				link.addEventListener("mouseleave", handleLinkMouseLeave, {
+					passive: true,
+				});
+				link.addEventListener("mousemove", handleLinkMouseMove, {
+					passive: true,
+				});
+			}
+		});
 
-        document.querySelectorAll(".js-interactable-button").forEach((button) => {
-            const isHidden = getComputedStyle(button).display === "none"
+		document.querySelectorAll(".js-interactable-button").forEach((button) => {
+			const isHidden = getComputedStyle(button).display === "none";
 
-            if (!isHidden) {
-                button.addEventListener("mouseenter", handleButtonMouseEnter, { passive: true })
-                button.addEventListener("mouseleave", handleButtonMouseLeave, { passive: true })
-            }
-        })
+			if (!isHidden) {
+				button.addEventListener("mouseenter", handleButtonMouseEnter, {
+					passive: true,
+				});
+				button.addEventListener("mouseleave", handleButtonMouseLeave, {
+					passive: true,
+				});
+			}
+		});
 
-        document.querySelectorAll("[class*=\"text-ms-0\"]:not(.is-hidden, .js-interactable-button), [class*=\"text-ms-1\"]:not(.is-hidden, .js-interactable-button)").forEach((text) => {
-            text.addEventListener("mouseout", handleTextMouseOut, { passive: true })
-            text.addEventListener("mouseover", handleTextMouseOver, { passive: true })
-        })
+		document
+			.querySelectorAll(
+				'[class*="text-ms-0"]:not(.font-monospace, .is-hidden, .js-interactable-button)'
+			)
+			.forEach((text) => {
+				text.addEventListener("mouseout", handleTextMouseOut, {
+					passive: true,
+				});
+				text.addEventListener("mouseover", handleTextMouseOver, {
+					passive: true,
+				});
+			});
 
-        return () => {
-            document.removeEventListener("scroll", handleScroll)
-            document.removeEventListener("mousedown", handleMouseDown)
-            document.removeEventListener("mousemove", handleMouseMove)
-            document.removeEventListener("mouseup", handleMouseUp)
+		return () => {
+			document.removeEventListener("scroll", handleScroll);
+			document.removeEventListener("mousedown", handleMouseDown);
+			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mouseup", handleMouseUp);
 
-            document.querySelectorAll(".js-interactable-link").forEach((link) => {
-                const isHidden = getComputedStyle(link).display === "none"
+			document.querySelectorAll(".js-interactable-link").forEach((link) => {
+				const isHidden = getComputedStyle(link).display === "none";
 
-                if (!isHidden) {
-                    link.removeEventListener("mouseenter", handleLinkMouseEnter)
-                    link.removeEventListener("mouseleave", handleLinkMouseLeave)
-                    link.removeEventListener("mousemove", handleLinkMouseMove)
-                }
-            })
+				if (!isHidden) {
+					link.removeEventListener("mouseenter", handleLinkMouseEnter);
+					link.removeEventListener("mouseleave", handleLinkMouseLeave);
+					link.removeEventListener("mousemove", handleLinkMouseMove);
+				}
+			});
 
-            document.querySelectorAll(".js-interactable-button").forEach((button) => {
-                const isHidden = getComputedStyle(button).display === "none"
+			document.querySelectorAll(".js-interactable-button").forEach((button) => {
+				const isHidden = getComputedStyle(button).display === "none";
 
-                if (!isHidden) {
-                    button.removeEventListener("mouseenter", handleButtonMouseEnter)
-                    button.removeEventListener("mouseleave", handleButtonMouseLeave)
-                }
-            })
+				if (!isHidden) {
+					button.removeEventListener("mouseenter", handleButtonMouseEnter);
+					button.removeEventListener("mouseleave", handleButtonMouseLeave);
+				}
+			});
 
-            document.querySelectorAll("[class*=\"text-ms-0\"]:not(.is-hidden, .js-interactable-button), [class*=\"text-ms-1\"]:not(.is-hidden, .js-interactable-button)").forEach((text) => {
-                text.removeEventListener("mouseout", handleTextMouseOut)
-                text.removeEventListener("mouseover", handleTextMouseOver)
-            })
-        }
+			document
+				.querySelectorAll(
+					'[class*="text-ms-0"]:not(.font-monospace, .is-hidden, .js-interactable-button)'
+				)
+				.forEach((text) => {
+					text.removeEventListener("mouseout", handleTextMouseOut);
+					text.removeEventListener("mouseover", handleTextMouseOver);
+				});
+		};
+	}, [cursorLocked]);
 
-    }, [cursorLocked])
+	return (
+		<div className={cursor} ref={ref}>
+			<div />
+		</div>
+	);
+};
 
-    return (
-        <div className={cursor} ref={ref}>
-            <div />
-        </div>
-    )
-}
-
-export default Cursor
+export default Cursor;
