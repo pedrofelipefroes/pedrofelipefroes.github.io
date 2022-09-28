5b9d03fb758ae33bf7ba4d8c7f158f498574b578
pedrofelipefroes <>
2022-09-28 16:26:45 -0300

    Updating Footer to use new utility classes.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,39 +1,27 @@
 import * as React from "react";
 import c from "classnames";
-import { StaticImage } from "gatsby-plugin-image";
 
-import { footer, footerNotice } from "./footer.module.css";
+import Signature from "../../images/signature.inline.svg";
+
+import { legal } from "./footer.module.css";
 
 const Footer = () => {
 	return (
-		<footer
-			className={c(
-				footer,
-				"align-items-end grid-golden-ratio text-ms-0 sp-inset-inline sp-inset-block-end-xs w-max-container"
-			)}
-		>
+		<footer className="l-golden-ratio s-inline-auto s-inset-inline s-inset-stack-2 txt-ms-0 u-align-items-end u-border-box w-max-container">
 			<p>
 				This site was fully designed and developed by myself, coffee, and{" "}
 				<a
 					href="https://open.spotify.com/playlist/0fAnZehFEjglZCHOiGGNcY?si=dae1004bfdf44b20"
-					className="color-on-bg js-interactable-link"
+					className="js-interactable-link"
 				>
-					a playlist filled with visual songs.
+					a playlist filled with visual&nbsp;songs.
 				</a>
 			</p>
-			<div className={footerNotice}>
-				<address className="sp-inline-end-sm">São Paulo, Brazil.</address>
-				<div>
-					<p className="sp-inline-end-sm">Froes © 2022.</p>
-					<StaticImage
-						src="../../images/signature.png"
-						alt="Froes handwritten signature in black."
-						formats={["auto", "webp", "avif"]}
-						layout="fixed"
-						loading="eager"
-						quality={100}
-						width={58}
-					/>
+			<div className={c(legal, "l-stack u-justify-content-space-between")}>
+				<address className="s-inline-3">São Paulo, Brazil.</address>
+				<div className="d-flex u-flex-grow-1 u-justify-content-space-between">
+					<p className="s-inline-3">Froes © 2022.</p>
+					<Signature className="u-flex-shrink-0" />
 				</div>
 			</div>
 		</footer>
