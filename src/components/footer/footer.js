883c3daf5752e11d5a2848cefdc99f88dc3346aa
pedrofelipefroes <>
2022-09-28 18:00:42 -0300

    Adding txt-ms-0 to paragraphs instead of the whole component.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,8 +7,8 @@ import { legal } from "./footer.module.css";
 
 const Footer = () => {
 	return (
-		<footer className="l-golden-ratio s-inline-auto s-inset-inline s-inset-stack-2 txt-ms-0 u-align-items-end u-border-box w-max-container">
-			<p>
+		<footer className="l-golden-ratio s-inline-auto s-inset-inline s-inset-stack-2 u-align-items-end u-border-box w-max-container">
+			<p className="txt-ms-0">
 				This site was fully designed and developed by myself, coffee, and{" "}
 				<a
 					href="https://open.spotify.com/playlist/0fAnZehFEjglZCHOiGGNcY?si=dae1004bfdf44b20"
@@ -18,9 +18,9 @@ const Footer = () => {
 				</a>
 			</p>
 			<div className={c(legal, "l-stack u-justify-content-space-between")}>
-				<address className="s-inline-3">São Paulo, Brazil.</address>
+				<address className="s-inline-3 txt-ms-0">São Paulo, Brazil.</address>
 				<div className="d-flex u-flex-grow-1 u-justify-content-space-between">
-					<p className="s-inline-3">Froes © 2022.</p>
+					<p className="s-inline-3 txt-ms-0">Froes © 2022.</p>
 					<Signature className="u-flex-shrink-0" />
 				</div>
 			</div>
