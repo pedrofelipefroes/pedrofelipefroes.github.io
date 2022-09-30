cb73f571cfb7ad425654d279c2a27c288a1673e1
pedrofelipefroes <>
2022-09-30 00:32:47 -0300

    Updating Button to use new CSS utility classes.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,17 +1,25 @@
-import React from "react"
-import c from "classnames"
-import PropTypes from "prop-types"
+import React from "react";
+import c from "classnames";
+import PropTypes from "prop-types";
 
-import { button } from "./button.module.css"
+import { button } from "./button.module.css";
 
 const Button = ({ children, ...props }) => {
-    return (
-        <button className={c(button, "color-on-bg js-interactable-button text-ms-0")} {...props}>{children}</button>
-    )
-}
+	return (
+		<button
+			className={c(
+				button,
+				"body d-inline-flex js-interactable-button p-relative u-align-items-center u-justify-content-center"
+			)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+};
 
 Button.propTypes = {
-    children: PropTypes.node.isRequired,
-}
+	children: PropTypes.node.isRequired,
+};
 
-export default Button
+export default Button;
