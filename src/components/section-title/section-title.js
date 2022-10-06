7703fd59b1de08272e566afb5d1160ca93718ad5
pedrofelipefroes <>
2022-10-05 22:20:24 -0300

    Updating Section Title with the new design.

--- src/components/section-title/section-title.js
+++ src/components/section-title/section-title.js
@@ -2,19 +2,20 @@ import * as React from "react";
 import c from "classnames";
 import PropTypes from "prop-types";
 
-import { Parallax } from "react-scroll-parallax";
-
 import { sectionTitle } from "./section-title.module.css";
 
 const SectionTitle = ({ label, ...other }) => {
 	return (
-		<Parallax opacity={[0.5, 0]} {...other}>
+		<div {...other}>
 			<h2
-				className={c(sectionTitle, "body d-flex txt-monospace txt-uppercase")}
+				className={c(
+					sectionTitle,
+					"caption caption--sparse d-inline-flex txt-monospace txt-uppercase"
+				)}
 			>
 				&#35;{label}
 			</h2>
-		</Parallax>
+		</div>
 	);
 };
 
