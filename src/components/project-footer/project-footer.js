4c80a8775e241c22dd859dc3f05eaa45f4fe4075
pedrofelipefroes <>
2022-10-19 18:18:36 -0300

    Updating Project Footer to remove golden ratio grid.

--- src/components/project-footer/project-footer.js
+++ src/components/project-footer/project-footer.js
@@ -5,9 +5,7 @@ import PropTypes from "prop-types";
 import { projectFooter, title } from "./project-footer.module.css";
 
 const ProjectFooter = ({ credits, summary }) => (
-	<footer
-		className={c(projectFooter, "l-golden-ratio l-golden-ratio--reverse")}
-	>
+	<div className={projectFooter}>
 		<div className="l-2col l-column-start-2--1366 s-inset-stack-10 u-divider">
 			<div className={c(title, "h-100")}>
 				<h2 className="title-3 s-stack-2 txt-serif">Summary</h2>
@@ -34,7 +32,7 @@ const ProjectFooter = ({ credits, summary }) => (
 				))}
 			</div>
 		</div>
-	</footer>
+	</div>
 );
 
 ProjectFooter.propTypes = {
