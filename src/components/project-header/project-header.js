d1acdbe860f0ab3210046ea0b46c5188ef2bf86d
pedrofelipefroes <>
2022-10-19 18:34:25 -0300

    Updating Project Header to include Parallax and better margins.

--- src/components/project-header/project-header.js
+++ src/components/project-header/project-header.js
@@ -2,11 +2,15 @@ import * as React from "react";
 import c from "classnames";
 import PropTypes from "prop-types";
 
+import { Parallax } from "react-scroll-parallax";
+
 import { projectHeader } from "./project-header.module.css";
 
 const ProjectHeader = ({ title }) => (
-	<header className={c(projectHeader, "s-stack-10 s-stack-20--768")}>
-		<h1 className="large-title txt-align-center txt-serif">{title}</h1>
+	<header className={c(projectHeader, "s-stack-15 s-stack-20--768")}>
+		<Parallax speed={-10}>
+			<h1 className="large-title txt-align-center txt-serif">{title}</h1>
+		</Parallax>
 	</header>
 );
 
