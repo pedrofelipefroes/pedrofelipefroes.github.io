db773d1a998a6ce3e15b4ea778a2512388c115b2
pedrofelipefroes <>
2023-04-27 16:05:28 -0300

    Updating Index page with link to PDF.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,8 @@ const IndexPage = () => {
 			<footer className="footer">
 				<div className="footer__content">
 					<h2 className="headline">
-						<span>Portfolio</span> <a>available here</a>.
+						<span>Portfolio</span>{" "}
+						<a href={`../froesdesign-portfolio.pdf`}>available here</a>.
 					</h2>
 					<div className="links">
 						<span className="subhead">
