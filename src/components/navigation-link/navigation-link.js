358dd6fb8ac151f59495c17d79ad3734793ee85c
pedrofelipefroes <>
2023-06-14 21:50:38 -0300

    Udating Navigation Link component.

--- src/components/navigation-link/navigation-link.js
+++ src/components/navigation-link/navigation-link.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import c from "classnames";
 import PropTypes from "prop-types";
 
 import { AnchorLink } from "gatsby-plugin-anchor-links";
@@ -7,24 +6,19 @@ import { OutboundLink } from "gatsby-plugin-google-gtag";
 
 import { navigationLink } from "./navigation-link.module.css";
 
-const NavigationLink = ({ children, className, to, ...other }) => {
+const NavigationLink = ({ children, to, ...other }) => {
 	const internal = /^\/(?!\/)/.test(to);
-	const classNames = c(
-		navigationLink,
-		className,
-		"caption d-inline-flex js-link p-relative txt-500 txt-monospace txt-uppercase"
-	);
 
 	if (internal) {
 		return (
-			<AnchorLink className={classNames} to={to} {...other}>
+			<AnchorLink className={navigationLink} to={to} {...other}>
 				{children}
 			</AnchorLink>
 		);
 	}
 
 	return (
-		<OutboundLink className={classNames} href={to} {...other}>
+		<OutboundLink className={navigationLink} href={to} {...other}>
 			{children}
 		</OutboundLink>
 	);
@@ -32,7 +26,6 @@ const NavigationLink = ({ children, className, to, ...other }) => {
 
 NavigationLink.propTypes = {
 	children: PropTypes.node,
-	className: PropTypes.string,
 	to: PropTypes.string.isRequired,
 };
 
