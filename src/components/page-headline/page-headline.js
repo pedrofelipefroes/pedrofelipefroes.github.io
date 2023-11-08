ff91f1b9a09cbef3d0049ed71630e991b9d2f62d
pedrofelipefroes <>
2023-11-07 23:55:38 -0300

    Adding Page Headline component.

--- src/components/page-headline/page-headline.js
+++ src/components/page-headline/page-headline.js
@@ -1,22 +1,37 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const PageHeadline = () => {
-	const wordmark = "Visuals";
-
 	return (
 		<section className="page-headline">
-			<div className="page-headline__subtitle">
-				<h3>Froes Designs</h3>
-			</div>
 			<div className="page-headline__title">
 				<h1>
-					{wordmark.split("").map((char) => (
-						<>{char}</>
-					))}
+					Fr
+					<span className="stylized-letter">
+						o{" "}
+						<span>
+							<StaticImage
+								alt="Hand drawn smiley to replace the letter 'o' from the name Froes."
+								className="doodle-1"
+								src="../../images/doodles/smile.png"
+							/>
+						</span>
+					</span>
+					{"\u00AD"}es De{"\u00AD"}signs Vis{"\u00AD"}u
+					<span>
+						{"\u00AD"}al
+						<span className="stylized-letter">
+							s
+							<span>
+								<StaticImage
+									alt="Handwritten 's' to decorate the word 'visuals.'"
+									className="doodle-2"
+									src="../../images/doodles/s.png"
+								/>
+							</span>
+						</span>
+					</span>
 				</h1>
-			</div>
-			<div className="page-headline__subtitle">
-				<h3>for Brands, Products &{"\u00A0"}Systems</h3>
 			</div>
 		</section>
 	);
