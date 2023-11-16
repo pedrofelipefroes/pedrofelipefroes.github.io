53d1f137cefbccc3cf6b693bd57f9d2471fdf2d8
pedrofelipefroes <>
2023-11-16 02:41:52 -0300

    Updating Poster Carousel.

--- src/components/poster-carousel/poster-carousel.js
+++ src/components/poster-carousel/poster-carousel.js
@@ -6,6 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const PosterCarousel = () => {
+	let autoplaySpeedValue = 2617;
+
 	const settings = {
 		arrows: false,
 		autoplay: true,
@@ -13,7 +15,6 @@ const PosterCarousel = () => {
 		dots: false,
 		infinite: true,
 		speed: 120000,
-		cssEase: "linear",
 		responsive: [
 			{
 				breakpoint: 1921,
@@ -34,36 +35,59 @@ const PosterCarousel = () => {
 		slidesToScroll: -1,
 	};
 
+	const innerSliderSettings = {
+		arrows: false,
+		autoplay: true,
+		dots: false,
+		draggable: false,
+		infinite: true,
+		pauseOnHover: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1,
+		swipe: false,
+	};
+
+	const innerSlider = (
+		<Slider autoplaySpeed={autoplaySpeedValue} {...innerSliderSettings}>
+			<div>
+				<StaticImage
+					src="../../images/artwork/poster_blue-banisters.jpg"
+					alt="Blue Banisters"
+				/>
+			</div>
+			<div>
+				<StaticImage
+					src="../../images/artwork/poster_cinnamon-girl.jpg"
+					alt="Cinnamon Girl"
+				/>
+			</div>
+			<div>
+				<StaticImage
+					src="../../images/artwork/poster_cruel-world.jpg"
+					alt="Cruel World"
+				/>
+			</div>
+			<div>
+				<StaticImage
+					src="../../images/artwork/poster_money-power-glory.jpg"
+					alt="Money Power Glory"
+				/>
+			</div>
+			<div>
+				<StaticImage src="../../images/artwork/poster_a-w.jpg" alt="A&W" />
+			</div>
+		</Slider>
+	);
+
 	return (
 		<div className="poster-carousel">
 			<Slider {...settings}>
-				<div>
-					<StaticImage
-						src="../../images/artwork/poster_blue-banisters.jpg"
-						alt="Blue Banisters"
-					/>
-				</div>
-				<div>
-					<StaticImage
-						src="../../images/artwork/poster_cinnamon-girl.jpg"
-						alt="Cinnamon Girl"
-					/>
-				</div>
-				<div>
-					<StaticImage
-						src="../../images/artwork/poster_cruel-world.jpg"
-						alt="Cruel World"
-					/>
-				</div>
-				<div>
-					<StaticImage
-						src="../../images/artwork/poster_money-power-glory.jpg"
-						alt="Money Power Glory"
-					/>
-				</div>
-				<div>
-					<StaticImage src="../../images/artwork/poster_a-w.jpg" alt="A&W" />
-				</div>
+				<div>{innerSlider}</div>
+				<div>{innerSlider}</div>
+				<div>{innerSlider}</div>
+				<div>{innerSlider}</div>
+				<div>{innerSlider}</div>
 			</Slider>
 		</div>
 	);
