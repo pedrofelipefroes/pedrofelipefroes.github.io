b4b63f312970cc26a9d08c46af7d167e7d37865a
pedrofelipefroes <>
2023-11-29 18:01:53 -0300

    Updating frame max height.

--- src/components/frame/frame.js
+++ src/components/frame/frame.js
@@ -2,24 +2,33 @@ import * as React from "react";
 import c from "classnames";
 import PropTypes from "prop-types";
 
-const Frame = ({ children, orientation, ...other }) => {
+const Frame = ({ align, children, justify, size, ...other }) => {
 	return (
 		<section
-			className={c("frame sans-serif", "frame__" + orientation)}
+			className={c(
+				"viewport viewport--fixed-height viewport--max-width",
+				"frame--" + align,
+				"frame--" + justify,
+				"frame--" + size
+			)}
 			{...other}
 		>
-			{children}
+			<div className="frame__content">{children}</div>
 		</section>
 	);
 };
 
 Frame.defaultProps = {
-	orientation: "landscape",
+	align: "top",
+	justify: "start",
+	size: "regular",
 };
 
 Frame.propTypes = {
+	justify: PropTypes.string,
 	children: PropTypes.node.isRequired,
-	orientation: PropTypes.string.isRequired,
+	justify: PropTypes.string,
+	size: PropTypes.string,
 };
 
 export default Frame;
