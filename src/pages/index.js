6ee2a2ab378063ae59f108d4c036958be890495b
pedrofelipefroes <>
2023-11-30 00:05:38 -0300

    Updating Index page for MVP.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,81 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+import React from "react";
+import Slider from "react-slick";
 
-import Frame from "../components/frame/frame";
+// import Frame from "../components/frame/frame";
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
-import ProjectTitle from "../components/project-description/project-description";
+import PageHeadline from "../components/page-headline/page-headline";
+// import PosterCarousel from "../components/poster-carousel/poster-carousel";
+import ProjectDescription from "../components/project-description/project-description";
 import Seo from "../components/seo/seo";
 
-import { navItemIsSelected } from "../components/nav-item/nav-item.module.css";
 import { StaticImage } from "gatsby-plugin-image";
-import PosterCarousel from "../components/poster-carousel/poster-carousel";
-import PageHeadline from "../components/page-headline/page-headline";
-import ProjectDescription from "../components/project-description/project-description";
 
-// import NavigationLink from "../components/navigation-link/navigation-link";
-// import { StaticImage } from "gatsby-plugin-image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const IndexPage = () => {
 	const projects = [
 		{
+			id: "ppt",
+			name: "Designing Perspective",
+			description:
+				"An AI-powered app to help you capture all of your moments and build a rich record of your\u00A0life.",
+			tags: ["Visual Design", "Product Design", "Branding"],
+			start: "2022",
+			end: "Ongoing",
+		},
+		{
+			id: "bds",
+			name: "Block Design System",
+			description:
+				"A conversion-driven landing page design system rooted in continuous\u00A0optimization.",
+			tags: ["Product Design", "Visual Design", "Front-end Development"],
+			start: "2020",
+			end: "2022",
+		},
+		{
+			id: "acr",
+			name: "Rebranding Avenue Code",
+			description:
+				"Refreshing Avenue Code brand with diversity, cohesiveness, and scalability in\u00A0mind.",
+			tags: ["Visual Design", "Branding"],
+			start: "2019",
+			end: "2020",
+		},
+		{
+			id: "cdr",
+			name: "Creative Direction",
+			description:
+				"Portfolio and concepts created for the Creative Direction course from Condé Nast College of Fashion &\u00A0Design.",
+			tags: ["Creative Direction", "Visual Design"],
+			start: "2019",
+			end: "ONGOING",
+		},
+		{
 			id: "pst",
 			name: "Poster Collection",
 			description:
-				"A collection of posters themed after diverse music artists and their inspiring\u00A0songs.",
-			tags: ["Art Direction", "Visual Design"],
-			start: 2020,
-			end: 2023,
+				"Translating inspiring song lyrics into sleek poster\u00A0design.",
+			tags: ["Visual Design"],
+			start: "2019",
+			end: "ONGOING",
 		},
-		{ id: "bds", name: "Block Design System" },
-		{ id: "acr", name: "Rebranding Avenue Code" },
-		{ id: "abt", name: "About Froes \u00B7 Design" },
 	];
 
-	useEffect(() => {
-		let observer = new IntersectionObserver(projectChange, {
-			rootMargin: "30px",
-			threshold: 0.5,
-		});
-
-		function clearSelected() {
-			document.querySelectorAll("[id$=trigger]").forEach((item) => {
-				item.classList.remove(navItemIsSelected);
-			});
-		}
-
-		function projectChange(entries, observer) {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					clearSelected();
-
-					if (
-						entry.target.id.includes("start") ||
-						entry.target.id.includes("end")
-					) {
-						let id = entry.target.id.split("-")[0] + "-trigger";
-						let el = document.getElementById(id);
-						if (el) el.classList.add(navItemIsSelected);
-					}
-				}
-			});
-		}
-
-		document
-			.querySelectorAll('[id$="start"], [id$="end"], [id^="author"]')
-			.forEach((item) => {
-				if (item) {
-					observer.observe(item);
-				}
-			});
-	}, []);
+	const projectCarouselSettings = {
+		arrows: true,
+		centerMode: true,
+		centerPadding: "2ex",
+		className: "center",
+		draggable: true,
+		dots: true,
+		easing: "ease-out",
+		infinite: false,
+		slidesToShow: 1.5,
+		// swipeToSlide: true,
+	};
 
 	return (
 		<>
@@ -88,46 +96,107 @@ const IndexPage = () => {
 				</section>
 				<Header projects={projects} />
 				<ul className="project-list">
-					<li className="project-list__item viewport viewport--max-width viewport--fixed-height">
-						<StaticImage
-							alt="teste"
-							src="../images/projects/bds/branded-landing-pages.jpg"
-						/>
-						<ProjectDescription
-							durationEnd={projects[0].end}
-							durationStart={projects[0].start}
-							subhead={projects[0].description}
-							title={projects[0].name}
-							tags={projects[0].tags}
-						/>
-					</li>
-					<li className="project-list__item viewport viewport--max-width viewport--fixed-height">
-						<StaticImage
-							alt="teste"
-							src="../images/projects/bds/branded-landing-pages.jpg"
-						/>
-						<ProjectDescription
-							durationEnd={projects[0].end}
-							durationStart={projects[0].start}
-							subhead={projects[0].description}
-							title={projects[0].name}
-							tags={projects[0].tags}
-						/>
-					</li>
-					<li className="project-list__item viewport viewport--max-width viewport--fixed-height">
-						<StaticImage
-							alt="teste"
-							src="../images/projects/bds/branded-landing-pages.jpg"
-						/>
-						<ProjectDescription
-							durationEnd={projects[0].end}
-							durationStart={projects[0].start}
-							subhead={projects[0].description}
-							title={projects[0].name}
-							tags={projects[0].tags}
-						/>
+					<li className="project-list__item">
+						<div className="viewport viewport--max-width">
+							<ProjectDescription
+								durationEnd={projects[0].end}
+								durationStart={projects[0].start}
+								subhead={projects[0].description}
+								title={projects[0].name}
+								tags={projects[0].tags}
+							/>
+						</div>
+						<figure>
+							<Slider {...projectCarouselSettings}>
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-9.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+							</Slider>
+						</figure>
 					</li>
 					<li className="project-list__item">
+						<div className="viewport viewport--max-width">
+							<ProjectDescription
+								durationEnd={projects[1].end}
+								durationStart={projects[1].start}
+								subhead={projects[1].description}
+								title={projects[1].name}
+								tags={projects[1].tags}
+							/>
+						</div>
+						<figure>
+							<Slider {...projectCarouselSettings}>
+								<StaticImage alt="teste" src="../images/mvp/avenuecode-1.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-9.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+							</Slider>
+						</figure>
+					</li>
+					<li className="project-list__item">
+						<div className="viewport viewport--max-width">
+							<ProjectDescription
+								durationEnd={projects[2].end}
+								durationStart={projects[2].start}
+								subhead={projects[2].description}
+								title={projects[2].name}
+								tags={projects[2].tags}
+							/>
+						</div>
+						<figure>
+							<Slider {...projectCarouselSettings}>
+								<StaticImage alt="teste" src="../images/mvp/avenuecode-1.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-9.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+							</Slider>
+						</figure>
+					</li>
+					<li className="project-list__item">
+						<div className="viewport viewport--max-width">
+							<ProjectDescription
+								durationEnd={projects[3].end}
+								durationStart={projects[3].start}
+								subhead={projects[3].description}
+								title={projects[3].name}
+								tags={projects[3].tags}
+							/>
+						</div>
+						<figure>
+							<Slider {...projectCarouselSettings}>
+								<StaticImage alt="teste" src="../images/mvp/avenuecode-1.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-9.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+							</Slider>
+						</figure>
+					</li>
+					<li className="project-list__item">
+						<div className="viewport viewport--max-width">
+							<ProjectDescription
+								durationEnd={projects[4].end}
+								durationStart={projects[4].start}
+								subhead={projects[4].description}
+								title={projects[4].name}
+								tags={projects[4].tags}
+							/>
+						</div>
+						<figure>
+							<Slider {...projectCarouselSettings}>
+								<StaticImage alt="teste" src="../images/mvp/avenuecode-1.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-9.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-4.png" />
+								<StaticImage alt="teste" src="../images/mvp/postclick-5.png" />
+							</Slider>
+						</figure>
+					</li>
+					{/* <li className="project-list__item">
 						<PosterCarousel />
 						<div className="viewport viewport--max-width">
 							<ProjectDescription
@@ -138,7 +207,7 @@ const IndexPage = () => {
 								tags={projects[0].tags}
 							/>
 						</div>
-					</li>
+					</li> */}
 				</ul>
 				<section className="about viewport viewport--max-width">
 					<div className="about__headline">
@@ -152,7 +221,7 @@ const IndexPage = () => {
 						<small>
 							I{"\u2019"}m a seasoned designer with 7+ years of experience
 							skilled at Visual Design, Product & UI Design, and design for
-							Digital Marketing.
+							Digital{"\u00A0"}Marketing.
 						</small>
 						<small>
 							Playing with typography and scaling visual systems are things I
@@ -162,9 +231,9 @@ const IndexPage = () => {
 						<small>
 							I{"\u2019"}m currently based in São Paulo, Brazil, but open to
 							relocation. Reach out for a chat and let{"\u2019"}s explore how we
-							can collaborate.
+							can{"\u00A0"}collaborate.
 						</small>
-						<small>
+						{/* <small>
 							Experience
 							<svg
 								width="2.5ex"
@@ -189,7 +258,7 @@ const IndexPage = () => {
 								<line x1="3" y1="15" x2="27" y2="15" />
 								<line x1="15" y1="3" x2="15" y2="27" />
 							</svg>
-						</small>
+						</small> */}
 					</div>
 					<small className="about__footnote">
 						Designed and developed by Froes.
